7553bdffabd3378c9d602cd93f1f1579be4183ac
Matthew Phillips <phillipsm2@gmail.com>
2011-02-01 22:39:04 -0500

    Made some fixes to the bash script

--- autopuz.bash
+++ autopuz.bash
@@ -3,7 +3,7 @@ mkdir /tmp/crossword
 cd /tmp/crossword
 todaydate=`date +%m-%d-%Y`
 
-wget http://www.chron.com/apps/games/xword/puzzles/
+wget http://www.chron.com/apps/games/xword/puzzles/today.puz
 
 #Processing unsolved puzzle
 nytconv today.puz >today.tex
@@ -11,7 +11,7 @@ latex today.tex
 dvips  -x 1400 today.dvi
 psnup  -l -2 -pletter today.ps today.wps
 echo \"quit\" >>today.wps
-lpr -P YOURPRINTER today.wps
+lpr -P YOUR-PRINTER today.wps
 
 #Processing solved puzzle
 nytconv2 today.puz >today2.tex
